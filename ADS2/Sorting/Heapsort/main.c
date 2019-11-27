d729992c7748bb118b9153be2aee17ea67022b96
riyyashahin <55278056+riyyashahin@users.noreply.github.com>
2019-11-27 22:07:16 +0530

    Create main.c

---
+++ ADS2/Sorting/Heapsort/main.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <assert.h>
+#include<stdio.h>
+#include "heap_sort.h"
+
+void test_sort( int32_t data[], uint32_t len) 
+{
+	uint32_t idx;
+	for (idx = 1; idx <= len; ++idx) {
+		assert (data[idx] <= data[idx+1]);		
+	}
+}
+
+void test_heap()
+{
+	int32_t data[] = { 0, 23, 54, 7, 19, 87, 56, 93, 61, 17, 73};
+	Heap heap = heap_new(data, 10);	
+	heap_insert(&heap, 47);
+	heap_test(&heap);
+	assert(heap_get_max(&heap) == 93);
+	assert(heap_extract_max(&heap) == 93);
+	heap_test(&heap);
+	assert(heap_size(&heap) == 10);
+
+	heap_sort(&heap);
+	test_sort(heap.data, 10);
+	uint32_t i;
+	for(i=0;i<11;i++)
+	{
+		printf("%d\t",heap.data[i]);
+	}
+	
+
+}
+
+int main()
+{
+	test_heap();
+	
+	return 0;
+
+}
